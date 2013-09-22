46e883dccf483c25a7af14c906861255b6959a84
kimji <jyaaa@naver.com>
2013-09-22 18:24:09 +0900

    login_page.php form삭제

--- application/views/login_page.php
+++ application/views/login_page.php
@@ -1,18 +1,14 @@
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="kor">
 <head>
-	<meta charset="utf-8">
-	<title>Login Page</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
+  <meta charset="utf-8">
+  <title>Login Page</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
     <meta name="author" content="">
 
-      <link href="./application/js/bootstrap-2.3.2/docs/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="application/js/bootstrap-2.3.2/docs/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="application/js/bootstrap-2.3.2/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 40px;
@@ -46,28 +42,14 @@
       }
 
     </style>
-    <link href="/application/js/bootstrap-2.3.2/docs/assets/css/bootstrap-responsive.css" rel="stylesheet">
-
-    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
-    <!--[if lt IE 9]>
-      <script src="../assets/js/html5shiv.js"></script>
-    <![endif]-->
-
-    <!-- Fav and touch icons -->
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
-      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
-                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
-                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
 </head>
 <body>
 
 <div class="container">
-	 <form class="form-signin">
-	<h2 class="form-signin-heading">Login Page</h2>
-	<input type="text" id="email" placeholder="이메일주소"/>
-	<input type="text" id="passwd" placeholder="비밀번호"/>
-	<button id="login" class="btn btn-large btn-primary">로그인</button>
+	 <h2 class="form-signin-heading">Login Page</h2>
+	 <input type="text" id="email" placeholder="이메일주소"/>
+	 <input type="text" id="passwd" placeholder="비밀번호"/>
+	 <button id="login" class="btn btn-large btn-primary">로그인</button>
 </div>
 
 </body>
